ef614d9881e66289d53b44c17b578452de623a49
augustquinlan <74929759+augustquinlan@users.noreply.github.com>
2020-11-26 06:13:23 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    devices.tellCameraTo(MesCameraEvent.TakePhoto)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        # # . # #
+        # . # . #
+        # # . # #
+        . # # # .
+        `)
+    devices.tellCameraTo(MesCameraEvent.ToggleFrontRear)
+})
 basic.forever(function () {
-	
+    bluetooth.startButtonService()
+    bluetooth.startLEDService()
 })
